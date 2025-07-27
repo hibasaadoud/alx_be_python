5d402a4669d670648f997e4ef16feb68c8fecf06
Hiba Saadoud <hibasaadoud235@gmail.com>
2025-07-27 04:10:52 -0700

    Fix return values for perform_operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,4 +12,17 @@ def perform_operation(num1: float, num2: float, operation: str):
     else:
         return "Error: Invalid operation"
 
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return None
+        return num1 / num2
+    else:
+        return None
 
