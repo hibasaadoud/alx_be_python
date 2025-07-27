e12dfe99694641d80c46ee6587d94b60ff28d6bc
Hiba Saadoud <hibasaadoud235@gmail.com>
2025-07-27 04:14:26 -0700

    Clean up duplicate functions and keep correct version

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,20 +7,6 @@ def perform_operation(num1: float, num2: float, operation: str):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            return "Error: Division by zero"
-        return num1 / num2
-    else:
-        return "Error: Invalid operation"
-
-def perform_operation(num1: float, num2: float, operation: str):
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
-        return num1 - num2
-    elif operation == 'multiply':
-        return num1 * num2
-    elif operation == 'divide':
-        if num2 == 0:
             return None
         return num1 / num2
     else:
